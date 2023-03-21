012c7d50544b5b3727c166146ca11d1c0b495a0c
elfourbo <elfourbo@42madrid.com>
2023-03-21 19:27:49 +0100

    Need cluster test before set as finished

--- p2/scripts/bootstrap.sh
+++ p2/scripts/bootstrap.sh
@@ -2,5 +2,8 @@
 
 apt-get update
 apt-get install curl -y
-#
+
+#Install and run kubernetes server
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-name mpivet-ps --node-ip 192.168.56.110
+
+kubectl apply -f /vagrant/manifest.yaml
