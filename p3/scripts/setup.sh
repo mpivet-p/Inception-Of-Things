f3745a4446181312aeb0e2497f8e5126894eb0e9
Maxime Pivet-pierre <mpivet-p@student.42.fr>
2023-03-26 15:24:04 +0200

    switched docker install to debian. Removing file with token at the end of the SW script

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -10,9 +10,10 @@ apt-get install \
     lsb-release -y
 
 #Setup docker repo:
-mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 #Install docker packages
 apt-get update
